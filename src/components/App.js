f73d270c91e6c4bfd94683cf416ffdeb2b0808e1
Kartik <Kartiknoriya56@gmail.com>
2026-02-08 19:30:04 +0530

    solved working

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+import SumCalculator from "./sumCalculator";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <SumCalculator />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SumCalculator.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+const SumCalculator = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const handleChange = (e) => {
+    setNumbers([...numbers, Number(e.target.value)]);
+  };
+  
+  useEffect(() => {
+    setTimeout(() => {
+      let total = 0;
+      for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+        if(total < 0){
+          total = 0;
+          setNumbers([])
+          return ;
+        }
+      }
+      setSum(total);
+    }, 0);
+  }, [numbers]);
+
+  return (
+    <div>
+      <h1>sum calculator</h1>
+
+      <input type="number" onChange={handleChange} />
+
+      <h3>sum: {sum}</h3>
+    </div>
+  );
+};
+
+export default SumCalculator;
